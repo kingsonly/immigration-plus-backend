ebcc5952452cfd4e75c2d64a667cd67d40083733
kingsley achumie <kingsonly13c@gmail.com>
2025-08-14 22:48:10 +0100

    first commit

---
+++ dist/src/api/new-test/routes/new-test.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * new-test router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::new-test.new-test');
