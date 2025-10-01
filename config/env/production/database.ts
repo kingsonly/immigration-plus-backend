f08176ba2457820ff7d0a12c5001375571458741
kingsley achumie <kingsonly13c@gmail.com>
2025-10-01 22:30:59 +0100

    wip

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,17 +1,18 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-module.exports = () => ({
+// config/env/production/database.js
+module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "mysql", // Use mysql2 driver in Strapi v5
     connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      pool: { min: 0, max: 10 },
-      acquireConnectionTimeout: 60000,
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: env.bool("DATABASE_SSL", false)
+        ? { rejectUnauthorized: false }
+        : false,
+    },
+    pool: { min: 0, max: 10 },
+    acquireConnectionTimeout: 60000,
   },
 });
