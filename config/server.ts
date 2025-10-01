e01ba61f5168c6c5ba3e9776b1d0bef985808a67
kingsley achumie <kingsonly13c@gmail.com>
2025-10-01 22:59:08 +0100

    wip

--- config/server.ts
+++ config/server.ts
@@ -7,9 +7,9 @@
 // });
 
 export default ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://c2c.skillzserver.com'),
+  url: env('PUBLIC_URL'),
   proxy: true,
-  host: '0.0.0.0',
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: { keys: env.array('APP_KEYS') },
 });
