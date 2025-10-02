ffc1dfd89a635026b1b8454e40e62c1335db5b21
kingsley achumie <kingsonly13c@gmail.com>
2025-10-02 15:50:46 +0100

    wip

---
+++ dist/build/CreatePage-DaEUzKV3.js
@@ -0,0 +1,1 @@
+import{aV as r,j as e,P as o}from"./strapi-D26zKyEy.js";import{E as i}from"./EditPage-QQtxgIhY.js";import"./selectors-BJuBCWo7.js";import"./useWebhooks--Y8l7xmT.js";const c=()=>{const s=r(t=>t.admin_app.permissions.settings?.webhooks.create);return e.jsx(o.Protect,{permissions:s,children:e.jsx(i,{})})};export{c as ProtectedCreatePage};
---
+++ dist/build/hooks-vxUOMug2.js
@@ -0,0 +1,1 @@
+import{n as s,o as e}from"./strapi-D26zKyEy.js";const t=e,a=s;export{t as a,a as u};
