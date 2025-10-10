e169dfa69fbf375783ca416975363b3c58a8359e
kingsley achumie <kingsonly13c@gmail.com>
2025-10-10 21:02:34 +0100

    wip

--- dist/src/api/resource/services/resource.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * resource service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::resource.resource');
--- dist/src/api/services-landing/services/services-landing.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * services-landing service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::services-landing.services-landing');
