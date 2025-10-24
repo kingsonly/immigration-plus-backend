9f6ad06feaeda11c08937b55f8092a4c35351d92
luckidubi <nwodochidubem1@gmail.com>
2025-10-23 17:08:46 -0700

    feat: added swagger api documentation and updated contact page api response

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default ({ env }) => ({
+  documentation: {
+    enabled: true,
+    config: {
+      info: {
+        title: "Immigration Plus API",
+        description: "API reference for Coming2Canada services",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: env("DOCS_BASE_URL", env("PUBLIC_URL", "http://localhost:1337")),
+          description: "Primary API server",
+        },
+      ],
+      externalDocs: {
+        description: "More details about Coming2Canada",
+        url: env("DOCS_EXTERNAL_URL", "https://cms.coming2canada.ca"),
+      },
+    },
+  },
+});
